f95dc949a43c809323bf2e7666f716b381275cb6
epwalsh <epwalsh10@gmail.com>
2018-05-18 17:13:50 -0500

    add basics

--- example.cpp
+++ example.cpp
@@ -3,13 +3,34 @@
 #include <csa.hpp>
 
 
+const int n = 10;
+
+
+double f(void* instance, double* x)
+{
+    return 0.;
+}
+
+
+void step(void* instance, double* x)
+{
+    for (int i = 0; i < n; ++i)
+        x[i] = 0.;
+}
+
+
+void progress(void* instance)
+{
+    return ;
+}
+
+
 int main()
 {
-    CSA::Solver<double> solver;
-    int n = 10;
-    double* x = new double[10];
+    CSA::Solver<double, double> solver;
+    double* x = new double[n];
 
-    int res = solver.minimize(n, x);
+    int res = solver.minimize(n, x, f, step, progress, nullptr);
 
     for (int i = 0; i < n; ++i)
         std::cout << x[i] << " ";
--- include/csa.hpp
+++ include/csa.hpp
@@ -4,14 +4,62 @@
 
 namespace CSA {
 
-template<typename Scalar>
+template<typename Scalar_x, typename Scalar_fx>
 class Solver
 {
 private:
 public:
-    inline int minimize(const int n, Scalar* x) {
+    /*
+     * =========================================================================
+     * Public member variables.
+     * =========================================================================
+     */
+
+    // The number of coupled annealing processes.
+    int n_process;
+    // The maximum number of iterations/steps.
+    int max_iterations = 10000;
+    // Specifies how many steps in between updates to the generation and
+    // acceptance temperatures.
+    int update_interval = 5;
+    // The initial value of the generation temperature.
+    Scalar_fx tgen_initial = 0.01;
+    // Determines the factor that tgen is multiplied by during each update.
+    Scalar_fx tgen_schedule = 0.99999;
+    // The initial value of the acceptance temperature.
+    Scalar_fx tacc_initial = 0.9;
+    // Determines the factor that `tacc` is multiplied by during each update.
+    Scalar_fx tagg_schedule = 0.95;
+    // The desired variance of the acceptance probabilities.
+    Scalar_fx desired_variance = 0.99;
+
+    /*
+     * =========================================================================
+     * Constructors.
+     * =========================================================================
+     */
+
+    Solver() {  };
+
+    /*
+     * =========================================================================
+     * Public member functions.
+     * =========================================================================
+     */
+
+    inline int minimize(const int n,
+                        Scalar_x* x,
+                        Scalar_fx (*fx)(void*, Scalar_x*),
+                        void (*step)(void*, Scalar_x*),
+                        void (*progress)(void*),
+                        void* instance)
+    {
+        Scalar_fx res = fx(instance, x);
+        printf("fx: %f\n", res);
+
         for (int i = 0; i < n; ++i)
             x[i] = 0;
+
         return 0;
     }
 };
